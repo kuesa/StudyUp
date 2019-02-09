08fd001c0d844c90e1cc531af08797232dc4298c
Chase Maguire <cmaguire@ucdavis.edu>
2019-02-09 00:46:41 -0800

    Testing on delete

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -132,5 +132,13 @@ class EventServiceImplTest {
 		List<Event> activeEvents = eventServiceImpl.getPastEvents();
 		assert activeEvents.contains(event);
 	}
-	
+	@Test
+	void testDeleteEvent_GoodCase() {
+		Event event = new Event();
+		event.setEventID(0);
+		event.setDate(new Date(0L));
+		eventServiceImpl.updateEvent(event);
+		eventServiceImpl.deleteEvent(0);
+		assert !(DataStorage.eventData.containsValue(event));
+	}
 }
