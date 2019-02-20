e5ce15f8855a65da218d93e15191bda7fa4a25d9
Chase Maguire <cmaguire@ucdavis.edu>
2019-02-19 21:07:42 -0800

    Finished

--- src/main/java/edu/studyup/util/DataStorage.java
+++ src/main/java/edu/studyup/util/DataStorage.java
@@ -1,5 +1,9 @@
 package edu.studyup.util;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import edu.studyup.entity.Event;
 /***
  * 
@@ -10,5 +14,5 @@ import edu.studyup.entity.Event;
  * 
  */
 public class DataStorage {
-	public static Map<Integer, Event> eventData = new HashMap<Integer, Event>();
+	public static final Map<Integer, Event> eventData = Collections.unmodifiableMap( new HashMap<Integer, Event>() );
 }
